293d831d201b58c8572f563e69ef3b974f1a1b87
triktron <ele.trik@hotmail.com>
2015-05-20 21:51:37 +0200

    tiny easteregg

--- server.js
+++ server.js
@@ -2,33 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var fs = require('fs');
 
-app.use(function(request, response, next) {
+app.use(function (request, response, next) {
     console.log("In comes a " + request.method + " to " + request.url);
     next();
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.contentType('text/html');
-    fs.readFile(__dirname + '/client/index.html', function(err, data) {
+    fs.readFile(__dirname + '/client/index.html', function (err, data) {
         if (err) throw err;
         res.send(data.toString());
     });
 });
-app.get('/main.js', function(req, res) {
+app.get('/main.js', function (req, res) {
     res.contentType('text/javascript');
-    fs.readFile(__dirname + '/client/main.js', function(err, data) {
+    fs.readFile(__dirname + '/client/main.js', function (err, data) {
         if (err) throw err;
         res.send(data.toString());
     });
 });
-app.get('/main.css', function(req, res) {
+app.get('/main.css', function (req, res) {
     res.contentType('text/css');
-    fs.readFile(__dirname + '/client/main.css', function(err, data) {
+    fs.readFile(__dirname + '/client/main.css', function (err, data) {
         if (err) throw err;
         res.send(data.toString());
     });
+});
+app.get('/triktron', function (req, res) {
+    res.send("I know<br>you are too;)");
 });
 
-http.listen(8080, '0.0.0.0', function() {
+http.listen(8080, '0.0.0.0', function () {
     console.log('listening on *:80');
 });
